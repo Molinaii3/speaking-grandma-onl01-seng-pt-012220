dae5e986f78332b3d9b6daa46915aac3274a93db
Michael Molina <l.13molina@gmail.com>
2020-02-04 05:11:03 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -16,7 +16,7 @@ def speak_to_grandma(phrase)
   
   if phrase == "I LOVE YOU GRANDMA!"
   "I LOVE YOU PUMPKIN!"
-  elsif phrase == phrase.upcase
+  elsif phrase == phrase.downcase
   "NO, NOT SINCE 1938!"
   else 
   "HUH?! SPEAK UP, SONNY!"
